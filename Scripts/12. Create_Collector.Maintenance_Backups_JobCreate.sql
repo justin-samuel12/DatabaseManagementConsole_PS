8f77c0a634e50feacc3355d7a6ed4049e8cb6c92
Justin Samuel <jsamuel12@gmail.com>
2014-10-14 14:18:20 -0400

    added db parameter to backup_job sp

--- Scripts/12. Create_Collector.Maintenance_Backups_JobCreate.sql
+++ Scripts/12. Create_Collector.Maintenance_Backups_JobCreate.sql
@@ -10,7 +10,6 @@ GO
 	DECLARE @Option varchar(256)= 'New';
 	DECLARE @Author varchar(256)= 'justin_samuel';
 	DECLARE @ObjectName varchar(256) = 'Collector.usp_Maintenance_Backups_JobCreate';
-	DECLARE @JobName varchar(256) = '$(Database_Name).Collector.Maintenance_Backups';
 	DECLARE @Description VARCHAR(100)='Creation of proc: '+ @ObjectName;	
 	DECLARE @ReleaseDate datetime = '10/1/2013';
 	DECLARE @DTNow DateTime2 = getdate();
@@ -24,93 +23,99 @@ BEGIN TRY
 -- =============================================
 -- Author:	Justin Samuel
 -- Create date: 3/1/2013
--- Description:	SP that will create job
+-- Description:	SP that will create job that will execute the backup process
 -- =============================================
 CREATE PROCEDURE ' + @ObjectName + '
+	@DatabaseName SYSNAME = ''$(Database_Name)''
 AS
 BEGIN TRY;
+		DECLARE @JobName varchar(256) = @DatabaseName+''.Collector.Maintenance_Backups'';
 		DECLARE @job_id binary(16);
-		SELECT @job_id = job_id FROM msdb.dbo.sysjobs WHERE name = ''' + @JobName + '''
+		DECLARE @SQL varchar(4000)='''';
+
+		SELECT @job_id = job_id FROM msdb.dbo.sysjobs WHERE name = '''' + @JobName + ''''
 		IF ( @job_id is not null ) begin EXEC msdb.dbo.sp_delete_job @job_id = @job_id, @delete_unused_schedule=1 end ;
 
-		BEGIN TRANSACTION;
-			DECLARE @ReturnCode INT = 0
-			IF NOT EXISTS (SELECT name FROM msdb.dbo.syscategories WHERE name=N''Database Backup'' AND category_class=1)
-				BEGIN
-					EXEC @ReturnCode = msdb.dbo.sp_add_category @class=N''JOB'', @type=N''LOCAL'', @name=N''Database Backup''
-					IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
-				END
+		SET @SQL = ''BEGIN TRANSACTION;
+					DECLARE @ReturnCode INT = 0
+					IF NOT EXISTS (SELECT name FROM msdb.dbo.syscategories WHERE name=N''''Database Backup'''' AND category_class=1)
+						BEGIN
+							EXEC @ReturnCode = msdb.dbo.sp_add_category @class=N''''JOB'''', @type=N''''LOCAL'''', @name=N''''Database Backup''''
+							IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
+						END
 
-		DECLARE @jobId BINARY(16)
-		EXEC @ReturnCode =  msdb.dbo.sp_add_job @job_name= N''' + @JobName + ''' , 
-				@enabled=1, 
-				@notify_level_eventlog=0, 
-				@notify_level_email=0, 
-				@notify_level_netsend=0, 
-				@notify_level_page=0, 
-				@delete_level=0, 
-				@description=N''Backup of database. Runs every hr and will create either full/differential or trans log.'', 
-				@category_name=N''Database Backup'', 
-				@owner_login_name=N''sa'', @job_id = @jobId OUTPUT
+						DECLARE @jobId BINARY(16)
+						EXEC @ReturnCode =  msdb.dbo.sp_add_job @job_name = '''''' + @JobName + '''''', 
+								@enabled=1, 
+								@notify_level_eventlog=0, 
+								@notify_level_email=0, 
+								@notify_level_netsend=0, 
+								@notify_level_page=0, 
+								@delete_level=0, 
+								@description=N''''Backup of database. Runs every hr and will create either full/differential or trans log.'''', 
+								@category_name=N''''Database Backup'''', 
+								@owner_login_name=N''''sa'''', @job_id = @jobId OUTPUT
 
-		IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
-		EXEC @ReturnCode = msdb.dbo.sp_add_jobstep @job_id=@jobId, @step_name=N''Execution of Maintenance Plan'', 
-				@step_id=1, 
-				@cmdexec_success_code=0, 
-				@on_success_action=3, 
-				@on_success_step_id=0, 
-				@on_fail_action=2, 
-				@on_fail_step_id=0, 
-				@retry_attempts=0, 
-				@retry_interval=0, 
-				@os_run_priority=0, @subsystem=N''TSQL'', 
-				@command=N''use [$(Database_Name)]
-		go
-		exec [Collector].[usp_Maintenance_Backups_Retrieve]'', 
-				@database_name=N''master'', 
-				@flags=0
-		IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
-		/****** Object:  Step [Create Restore Scripts]    Script Date: 10/22/2013 3:41:53 PM ******/
-		EXEC @ReturnCode = msdb.dbo.sp_add_jobstep @job_id=@jobId, @step_name=N''Create Restore Scripts'', 
-				@step_id=2, 
-				@cmdexec_success_code=0, 
-				@on_success_action=1, 
-				@on_success_step_id=0, 
-				@on_fail_action=2, 
-				@on_fail_step_id=0, 
-				@retry_attempts=0, 
-				@retry_interval=0, 
-				@os_run_priority=0, @subsystem=N''TSQL'', 
-				@command=N''use [$(Database_Name)]
-		go
-		exec [Collector].[usp_Maintenance_Backups_CreateRestoreScripts]'', 
-				@database_name=N''master'', 
-				@flags=0
-		IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
-		EXEC @ReturnCode = msdb.dbo.sp_update_job @job_id = @jobId, @start_step_id = 1
-		IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
+						IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
+						EXEC @ReturnCode = msdb.dbo.sp_add_jobstep @job_id=@jobId, @step_name=N''''Execution of Maintenance Plan'''', 
+								@step_id=1, 
+								@cmdexec_success_code=0, 
+								@on_success_action=3, 
+								@on_success_step_id=0, 
+								@on_fail_action=2, 
+								@on_fail_step_id=0, 
+								@retry_attempts=0, 
+								@retry_interval=0, 
+								@os_run_priority=0, @subsystem=N''''TSQL'''', 
+								@command=N''''use ['' + @DatabaseName +'']
+						go
+						exec [Collector].[usp_Maintenance_Backups_Retrieve]'''', 
+								@database_name=N''''master'''', 
+								@flags=0
+						IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
+						/****** Object:  Step [Create Restore Scripts]    Script Date: 10/22/2013 3:41:53 PM ******/
+						EXEC @ReturnCode = msdb.dbo.sp_add_jobstep @job_id=@jobId, @step_name=N''''Create Restore Scripts'''', 
+								@step_id=2, 
+								@cmdexec_success_code=0, 
+								@on_success_action=1, 
+								@on_success_step_id=0, 
+								@on_fail_action=2, 
+								@on_fail_step_id=0, 
+								@retry_attempts=0, 
+								@retry_interval=0, 
+								@os_run_priority=0, @subsystem=N''''TSQL'''', 
+								@command=N''''use ['' + @DatabaseName +'']
+						go
+						exec [Collector].[usp_Maintenance_Backups_CreateRestoreScripts]'''', 
+								@database_name=N''''master'''', 
+								@flags=0
+						IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
+						EXEC @ReturnCode = msdb.dbo.sp_update_job @job_id = @jobId, @start_step_id = 1
+						IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
 
-		EXEC @ReturnCode = msdb.dbo.sp_add_jobschedule @job_id=@jobId, @name=N''' + @JobName + ''', 
-				@enabled=1, 
-				@freq_type=4, 
-				@freq_interval=1, 
-				@freq_subday_type=8, 
-				@freq_subday_interval=1, 
-				@freq_relative_interval=0, 
-				@freq_recurrence_factor=0, 
-				@active_start_date=20131022, 
-				@active_end_date=99991231, 
-				@active_start_time=1500, 
-				@active_end_time=235959, 
-				@schedule_uid=N''55ef72c9-902e-4aa7-9b51-0e6d3a3b42d6''
-		IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
-		EXEC @ReturnCode = msdb.dbo.sp_add_jobserver @job_id = @jobId, @server_name = N''(local)''
-		IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
-		COMMIT TRANSACTION
-		GOTO EndSave
-		QuitWithRollback:
-			IF (@@TRANCOUNT > 0) ROLLBACK TRANSACTION
-		EndSave:
+						EXEC @ReturnCode = msdb.dbo.sp_add_jobschedule @job_id=@jobId, @name = '''''' + @JobName + '''''', 
+								@enabled=1, 
+								@freq_type=4, 
+								@freq_interval=1, 
+								@freq_subday_type=8, 
+								@freq_subday_interval=1, 
+								@freq_relative_interval=0, 
+								@freq_recurrence_factor=0, 
+								@active_start_date=20131022, 
+								@active_end_date=99991231, 
+								@active_start_time=1500, 
+								@active_end_time=235959, 
+								@schedule_uid=N''''55ef72c9-902e-4aa7-9b51-0e6d3a3b42d6''''
+						IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
+						EXEC @ReturnCode = msdb.dbo.sp_add_jobserver @job_id = @jobId, @server_name = N''''(local)''''
+						IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
+						COMMIT TRANSACTION
+						GOTO EndSave
+						QuitWithRollback:
+							IF (@@TRANCOUNT > 0) ROLLBACK TRANSACTION
+						EndSave:'';
+
+				EXEC (@SQL);
 END TRY
 BEGIN CATCH
 	DECLARE @ProcedureName		SYSNAME			= QUOTENAME(OBJECT_SCHEMA_NAME(@@PROCID)) +''.'' + QUOTENAME(object_name(@@PROCID))
--- Scripts/13. Create_Collector.Maintenance_Backups_DeleteFolderContents_JobCreate.sql
+++ Scripts/13. Create_Collector.Maintenance_Backups_DeleteFolderContents_JobCreate.sql
@@ -11,7 +11,6 @@ GO
 	DECLARE @Option varchar(256)= 'New';
 	DECLARE @Author varchar(256)= 'justin_samuel';
 	DECLARE @ObjectName varchar(256) = 'Collector.usp_Maintenance_Backups_DeleteFolderContents_JobCreate';
-	DECLARE @JobName varchar(256) = '$(Database_Name).Collector.Maintenance_Backups_DeleteFolderContents';
 	DECLARE @Description VARCHAR(100)='Creation of proc: '+ @ObjectName;	
 	DECLARE @ReleaseDate datetime = '10/1/2013';
 	DECLARE @DTNow DateTime2 = getdate();
@@ -25,76 +24,84 @@ BEGIN TRY
 -- =============================================
 -- Author:	Justin Samuel
 -- Create date: 3/1/2013
--- Description:	SP that will create job
+-- Description:	SP that will create job to delete folder contents
 -- =============================================
 CREATE PROCEDURE ' + @ObjectName + '
+	@DatabaseName SYSNAME = ''$(Database_Name)''
 AS
 BEGIN TRY;
+	DECLARE @JobName varchar(256) = @DatabaseName+''.Collector.Maintenance_Backups_DeleteFolderContents'';
 	DECLARE @job_id binary(16);
-	SELECT @job_id = job_id FROM msdb.dbo.sysjobs WHERE name = ''' + @JobName + '''
+	DECLARE @SQL varchar(4000)='''';
+	
+	SELECT @job_id = job_id FROM msdb.dbo.sysjobs WHERE name = '''' + @JobName + ''''
 	IF ( @job_id is not null ) begin EXEC msdb.dbo.sp_delete_job @job_id = @job_id, @delete_unused_schedule=1 end ;
 
-	BEGIN TRANSACTION;
-		DECLARE @ReturnCode INT = 0
-		IF NOT EXISTS (SELECT name FROM msdb.dbo.syscategories WHERE name=N''Database Maintenance'' AND category_class=1)
-			BEGIN
-				EXEC @ReturnCode = msdb.dbo.sp_add_category @class=N''JOB'', @type=N''LOCAL'', @name=N''Database Maintenance''
-				IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
-			END
+	SET @SQL = ''BEGIN TRANSACTION;
+				DECLARE @ReturnCode INT = 0
+				IF NOT EXISTS (SELECT name FROM msdb.dbo.syscategories WHERE name=N''''Database Backup'''' AND category_class=1)
+				BEGIN
+					EXEC @ReturnCode = msdb.dbo.sp_add_category @class=N''''JOB'''', @type=N''''LOCAL'''', @name=N''''Database Backup''''
+					IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
+				END
 
-	DECLARE @jobId BINARY(16)
-	EXEC @ReturnCode =  msdb.dbo.sp_add_job @job_name= N''' + @JobName + ''' , 
-			@enabled=1, 
-			@notify_level_eventlog=0, 
-			@notify_level_email=0, 
-			@notify_level_netsend=0, 
-			@notify_level_page=0, 
-			@delete_level=0, 
-			@description=N''Delete previous files not being used'', 
-			@category_name=N''Database Maintenance'', 
-			@owner_login_name=N''sa'', @job_id = @jobId OUTPUT
 
-	IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
-	EXEC @ReturnCode = msdb.dbo.sp_add_jobstep @job_id=@jobId, @step_name=N''Delete Files'', 
-			@step_id=1, 
-			@cmdexec_success_code=0, 
-			@on_success_action=1, 
-			@on_success_step_id=0, 
-			@on_fail_action=2, 
-			@on_fail_step_id=0, 
-			@retry_attempts=0, 
-			@retry_interval=0,  
-			@os_run_priority=0, @subsystem=N''TSQL'', 
-			@command=N''use [$(Database_Name)]
-	go
-	exec [Collector].[usp_Maintenance_Backups_DeleteFolderContents]'', 
-			@database_name=N''master'', 
-			@flags=0
-	IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
-	EXEC @ReturnCode = msdb.dbo.sp_update_job @job_id = @jobId, @start_step_id = 1
-	IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
+			DECLARE @jobId BINARY(16)
+			EXEC @ReturnCode =  msdb.dbo.sp_add_job @job_name = '''''' + @JobName + '''''', 
+					@enabled=1, 
+					@notify_level_eventlog=0, 
+					@notify_level_email=0, 
+					@notify_level_netsend=0, 
+					@notify_level_page=0, 
+					@delete_level=0, 
+					@description=N''''Delete previous files not being used'''', 
+					@category_name=N''''Database Backup'''', 
+					@owner_login_name=N''''sa'''', @job_id = @jobId OUTPUT
 
-	EXEC @ReturnCode = msdb.dbo.sp_add_jobschedule @job_id=@jobId, @name=N''' + @JobName + ''', 
-			@enabled=1, 
-			@freq_type=4, 
-			@freq_interval=1, 
-			@freq_subday_type=1, 
-			@freq_subday_interval=0, 
-			@freq_relative_interval=0, 
-			@freq_recurrence_factor=0, 
-			@active_start_date=20131022, 
-			@active_end_date=99991231, 
-			@active_start_time=0, 
-			@active_end_time=235959, 
-			@schedule_uid=N''824c2728-313c-4a01-9b13-782f5ed1f790''
-	IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
-	EXEC @ReturnCode = msdb.dbo.sp_add_jobserver @job_id = @jobId, @server_name = N''(local)''
-	IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
-	COMMIT TRANSACTION
-	GOTO EndSave
-	QuitWithRollback:
-		IF (@@TRANCOUNT > 0) ROLLBACK TRANSACTION
-	EndSave:
+			IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
+			EXEC @ReturnCode = msdb.dbo.sp_add_jobstep @job_id=@jobId, @step_name=N''''Delete Files'''', 
+					@step_id=1, 
+					@cmdexec_success_code=0, 
+					@on_success_action=1, 
+					@on_success_step_id=0, 
+					@on_fail_action=2, 
+					@on_fail_step_id=0, 
+					@retry_attempts=0, 
+					@retry_interval=0,  
+					@os_run_priority=0, @subsystem=N''''TSQL'''',  
+					@command=N''''use ['' + @DatabaseName +'']
+			go
+					exec [Collector].[usp_Maintenance_Backups_DeleteFolderContents]'''', 
+					@database_name=N''''master'''', 
+					@flags=0				
+			IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
+			EXEC @ReturnCode = msdb.dbo.sp_update_job @job_id = @jobId, @start_step_id = 1
+			IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
+
+			EXEC @ReturnCode = msdb.dbo.sp_add_jobschedule @job_id=@jobId, @name = '''''' + @JobName + '''''', 
+					@enabled=1, 
+					@freq_type=4, 
+					@freq_interval=1, 
+					@freq_subday_type=1, 
+					@freq_subday_interval=0, 
+					@freq_relative_interval=0, 
+					@freq_recurrence_factor=0, 
+					@active_start_date=20131022, 
+					@active_end_date=99991231, 
+					@active_start_time=0, 
+					@active_end_time=235959, 
+					@schedule_uid=N''''824c2728-313c-4a01-9b13-782f5ed1f790''''
+			IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
+			EXEC @ReturnCode = msdb.dbo.sp_add_jobserver @job_id = @jobId, @server_name = ''''(local)''''
+			IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
+			COMMIT TRANSACTION
+			GOTO EndSave
+			QuitWithRollback:
+				IF (@@TRANCOUNT > 0) ROLLBACK TRANSACTION
+			EndSave:'';
+
+			EXEC (@SQL);
+
 END TRY
 BEGIN CATCH
 	DECLARE @ProcedureName		SYSNAME			= QUOTENAME(OBJECT_SCHEMA_NAME(@@PROCID)) +''.'' + QUOTENAME(object_name(@@PROCID))
@@ -108,7 +115,7 @@ BEGIN CATCH
 END CATCH;
 ';
 	--PRINT @SQL
-	 EXEC (@SQL)
+	EXEC (@SQL)
 	 
 	 -- 3. insert into [Config].[t_VersionControl]
 	 EXEC Configuration.usp_VersionControl_Merge @VersionNumber = @VersionNumber, @ScriptName = '$(File_Name)', @Author = @Author, 
