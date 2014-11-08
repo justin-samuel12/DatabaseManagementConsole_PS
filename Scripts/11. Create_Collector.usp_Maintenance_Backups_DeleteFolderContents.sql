9d6ab6cd777f1ced1d652e39304f1141b9c8c1bc
Justin Samuel <jsamuel12@gmail.com>
2014-11-08 17:54:59 -0500

    new Delete sp
    changed delete folder content sp from multiple lines to 1 lines; use of
    powershell syntax

--- Scripts/11. Create_Collector.usp_Maintenance_Backups_DeleteFolderContents.sql
+++ Scripts/11. Create_Collector.usp_Maintenance_Backups_DeleteFolderContents.sql
@@ -28,130 +28,19 @@ CREATE PROCEDURE ' + @ObjectName + '
 	@DaysToRetain INT = 4 -- how many days back to start deletion (default is to keep 4 days).
 AS
 BEGIN
-	SET NOCOUNT ON; SET XACT_ABORT ON;
-/*********************************************** VARIABLES *********************************************************************************/
+	SET NOCOUNT ON; 	
 	DECLARE @DateToRemove				DateTime =  DATEADD(DAY, DATEDIFF(DAY, 0 , GETDATE()) - @DaysToRetain,0 ); -- actual date
 	DECLARE @Backupfolderlocation		VARCHAR(100) = [Configuration].[svfn_DefaultFolderLocation_Get](''BackupDirectory''); -- backupfolder local
-	DECLARE @Command					VARCHAR(256) = ''DIR "''+ @Backupfolderlocation + ''" /B /T:C'';
-	DECLARE @SQL						NVARCHAR(4000) = NULL;
-	DECLARE @RC							INT;
-	DECLARE @Tmp						TABLE (returnval NVARCHAR(1000), rownum INT IDENTITY(1,1));
-	DECLARE @DirectoryName				NVARCHAR(1000);
-	DECLARE @DirectoryLoopCounter		INT = 1;
-	DECLARE @DirectoryFilesTotalCount	INT = 0;
-	DECLARE @FileDetails				TABLE
-											(
-												Name             VARCHAR(500), --File Name and Extension
-												[Path]           VARCHAR(500), --Full path including file name
-												DateCreated      DATETIME,     --Date file was created
-												DateLastAccessed DATETIME,     --Date file was last read
-												DateLastModified DATETIME,     --Date file was last written to
-												Attributes       INT,          --Read only, Compressed, Archived
-												ArchiveBit       AS CASE WHEN Attributes&  32=32   THEN 1 ELSE 0 END,
-												CompressedBit    AS CASE WHEN Attributes&2048=2048 THEN 1 ELSE 0 END,
-												ReadOnlyBit      AS CASE WHEN Attributes&   1=1    THEN 1 ELSE 0 END,
-												Size             INT,          --File size in bytes
-												[Type]           VARCHAR(100)  --Long Windows file type (eg.''Text Document'',etc)
-											);
-
-	IF OBJECT_ID(''TempDB..#DirectoryFilesInformaton'') IS NOT NULL BEGIN DROP TABLE #DirectoryFilesInformaton END;
-	CREATE TABLE #DirectoryFilesInformaton (RowNum INT IDENTITY(1,1), Name VARCHAR(500) PRIMARY KEY CLUSTERED, Depth  BIT, IsFile BIT)					
-/****************************************************************************************************************************************/
-	-- 1. Populate Temp Table with the contents of the outfiles directory
-		INSERT @tmp EXEC master..xp_cmdshell @Command
+	DECLARE @SQL						NVARCHAR(4000) = ''FORFILES /p '' + @Backupfolderlocation + '' /s /m *.* /d -'' + cast(@DaysToRetain as varchar(5)) + '' /c "CMD /C del /Q /F @FILE"'';
 	
-	-- 2. Remove any unwanted data
-		DELETE FROM @tmp where returnval is null;
-	
-	-- 3. Loop thru each directory and get data
-		WHILE @DirectoryLoopCounter <= ( SELECT COUNT( Rownum ) FROM @tmp )
-			BEGIN
-				DECLARE @ObjFile		  INT;				  --File object
-				DECLARE @ObjFileSystem	  INT;				  --File System Object  
-				DECLARE @Attributes       INT		   = 0	  --Read only, Hidden, Archived, etc, as a bit map
-				DECLARE @DateCreated      DATETIME			  --Date file was created
-				DECLARE @DateLastAccessed DATETIME			  --Date file was last read (accessed)
-				DECLARE @DateLastModified DATETIME			  --Date file was last written to
-				DECLARE @Name             VARCHAR(500) = NULL --File Name and Extension
-				DECLARE @Path             VARCHAR(500) = NULL --Full path including file name
-				DECLARE @Size             INT          = 0	  --File size in bytes
-				DECLARE @Type             VARCHAR(100) = NULL --Long Windows file type (eg.''Text Document'',etc)
-				DECLARE @CurrentFileName  VARCHAR(500);
-				DECLARE @FileCount		  INT		   = 0; 
-				DECLARE @FileLoopCounter  INT          = 1;		
-				SELECT @DirectoryName = @Backupfolderlocation + Returnval +''\'' FROM @tmp WHERE rownum = @DirectoryLoopCounter;
-				-- create temp table to get all files for current dir
-				INSERT INTO #DirectoryFilesInformaton (Name, Depth, IsFile) EXEC Master.dbo.xp_DirTree @DirectoryName,1,1; -- get all files for that directory
-				SET @FileCount = @@ROWCOUNT;				
-				--=================================================================================================
-				--      Get the properties for each file.  This is one of the few places that a WHILE
-				--      loop is required in T-SQL.
-				--=================================================================================================
-				--===== Create a file system object and remember the "handle"
-				EXEC dbo.sp_OACreate ''Scripting.FileSystemObject'', @ObjFileSystem OUT
-					WHILE @FileLoopCounter <= @FileCount
-						BEGIN
-							SELECT @CurrentFileName = @DirectoryName + name FROM #DirectoryFilesInformaton WHERE RowNum = @FileLoopCounter and IsFile = 1;
-							--===== Create an object for the path/file and remember the "handle"
-							EXEC dbo.sp_OAMethod @ObjFileSystem,''GetFile'', @ObjFile OUT, @CurrentFileName
-                
-							--===== Get the all the required attributes for the file itself
-							EXEC dbo.sp_OAGetProperty @ObjFile, ''Path'',             @Path             OUT
-							EXEC dbo.sp_OAGetProperty @ObjFile, ''Name'',             @Name             OUT
-							EXEC dbo.sp_OAGetProperty @ObjFile, ''DateCreated'',      @DateCreated      OUT
-							EXEC dbo.sp_OAGetProperty @ObjFile, ''DateLastAccessed'', @DateLastAccessed OUT
-							EXEC dbo.sp_OAGetProperty @ObjFile, ''DateLastModified'', @DateLastModified OUT
-							EXEC dbo.sp_OAGetProperty @ObjFile, ''Attributes'',       @Attributes       OUT
-							EXEC dbo.sp_OAGetProperty @ObjFile, ''Size'',             @Size             OUT
-							EXEC dbo.sp_OAGetProperty @ObjFile, ''Type'',             @Type             OUT
-        
-							--===== Insert the file details into the return table        
-							INSERT @FileDetails ([Path], Name,  DateCreated, DateLastAccessed, DateLastModified, Attributes, Size, [Type])
-							SELECT @Path, @Name,@DateCreated, @DateLastAccessed,@DateLastModified,@Attributes,@Size,@Type
-
-							-- set Total count
-							SET @FileLoopCounter +=1
-						END;
-							
-					--===== House keeping, destroy and drop the file objects to keep memory leaks from happening
-					   TRUNCATE TABLE #DirectoryFilesInformaton;				
-					   EXEC sp_OADestroy @ObjFileSystem
-					   EXEC sp_OADestroy @ObjFile
-			SET @DirectoryLoopCounter +=1
-		END;
-
-		-- 4. loop thru to delete files
-		DELETE FROM @FileDetails where @DateToRemove < @DateCreated -- delete all irrvelant files that are not to be deleted
-		DECLARE @t_FilesToDelete TABLE ( RowNum INT IDENTITY(1,1) PRIMARY KEY CLUSTERED, FullFileName NVARCHAR(4000))
-		INSERT @t_FilesToDelete (FullFileName) SELECT Path FROM @FileDetails
-		SET @DirectoryFilesTotalCount = @@ROWCOUNT;
+	EXEC xp_cmdshell @SQL;
 				
-		IF ( @DirectoryFilesTotalCount > 0 )
-			BEGIN
-				SET @DirectoryLoopCounter = 1;
-				WHILE @DirectoryLoopCounter <= @DirectoryFilesTotalCount
-					BEGIN
-						SELECT @CurrentFileName = FullFileName from @t_FilesToDelete Where RowNum = @DirectoryLoopCounter;
-						SET @SQL =''''; -- reuse the @sql variable 
-						SET @SQL = ''DEL "'' + @CurrentFileName + ''" /F /Q''  
-
-						EXECUTE @RC = master.dbo.xp_cmdshell @SQL 
-										
-						IF @rc <> 0 
-							BEGIN 
-								RAISERROR (''Error deleting file:  %s'', 16, 1, @CurrentFileName);		
-								RETURN;
-							END
-
-						SET @DirectoryLoopCounter +=1
-					END;
-			END;		
-		-- 5. Delete from Backupset tables / msdb backupset
-		DELETE FROM [Collector].[t_Backupset] WHERE DATEADD(DAY, DATEDIFF(DAY, 0 , CreateDatetime),0 ) <= @DateToRemove;
-		DELETE FROM [Collector].[t_BackupsetOperation] WHERE DATEADD(DAY, DATEDIFF(DAY, 0 , CreateDatetime),0 ) <= @DateToRemove;
-		DELETE FROM [Collector].[t_BackupsetRestoreScripts] WHERE DATEADD(DAY, DATEDIFF(DAY, 0 , CreateDatetime),0 ) <= @DateToRemove;
+	-- Delete from Backupset tables / msdb backupset
+	DELETE FROM [Collector].[t_Backupset] WHERE DATEADD(DAY, DATEDIFF(DAY, 0 , CreateDatetime),0 ) <= @DateToRemove;
+	DELETE FROM [Collector].[t_BackupsetOperation] WHERE DATEADD(DAY, DATEDIFF(DAY, 0 , CreateDatetime),0 ) <= @DateToRemove;
+	DELETE FROM [Collector].[t_BackupsetRestoreScripts] WHERE DATEADD(DAY, DATEDIFF(DAY, 0 , CreateDatetime),0 ) <= @DateToRemove;
 		
-		EXEC MSDB..SP_DELETE_BACKUPHISTORY @DateToRemove;
+	EXEC MSDB..SP_DELETE_BACKUPHISTORY @DateToRemove;
 END		
 ';
 	 --PRINT @SQL
